d2f49773326ed3cf657053a9691d09478e97dc9f
Tuan Bui <ntuanb@gmail.com>
2020-05-10 15:41:03 +1000

    Add docs to load example

--- v0_03/index.php
+++
@@ -1,7 +0,0 @@
-<div id="game"></div>
-
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="lib/keydrown-master/dist/keydrown.min.js"></script>
-<script src="lib/pixi.js-master/bin/pixi.dev.js"></script>
-<script src="js/howler.min.js"></script>
-<script src="spaceshooter_v0_02.js"></script>
